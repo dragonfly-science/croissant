b80a4efe1942282390c4cd6c44007650f899d309
dan morrison <dan@rabidtech.co.nz>
2019-12-10 08:40:02 +1300

    Set up project

---
+++ Gemfile
@@ -0,0 +1,47 @@
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "2.6.3"
+
+gem "rails", "6.0.1"
+gem "puma", "~> 4.1"
+gem "pg", ">= 0.18"
+gem 'dotenv-rails', require: "dotenv/rails-now"
+gem "bootsnap", ">= 1.4.2", require: false
+gem "sassc-rails"
+gem "redis", "~> 4.0"
+gem "sidekiq", ">= 4.2.0"
+gem "webpacker", "~> 4.0"
+
+gem "rack-canonical-host", "~> 0.2.3"
+
+group :development do
+  gem "bundler-audit", require: false
+  gem "brakeman", require: false
+  gem "simplecov", require: false
+  gem "rubocop", ">= 0.70.0", require: false
+  gem "rubocop-performance", require: false
+  gem "rubocop-rails", require: false
+  gem "letter_opener"
+
+  # Required in Rails 5 by ActiveSupport::EventedFileUpdateChecker
+  gem "listen", ">= 3.0.5"
+  gem "overcommit", ">= 0.37.0", require: false
+end
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem "pry-rails"
+  gem "pry-byebug"
+end
+
+group :test do
+  gem "capybara", ">= 2.15"
+  gem "mock_redis"
+  gem "selenium-webdriver"
+  gem "webdrivers"
+
+gem "lighthouse-matchers"
+gem "axe-matchers"
+end
---
+++ config/routes.rb
@@ -0,0 +1,37 @@
+Rails.application.routes.draw do
+  ##
+  # Workaround a "bug" in lighthouse CLI
+  #
+  # Lighthouse CLI (versions 5.4 - 5.6 tested) issues a `GET /asset-manifest.json`
+  # request during its run - the URL seems to be hard-coded. This file does not
+  # exist so, during tests, your test will fail because rails will die with a 404.
+  #
+  # Lighthouse run from Chrome Dev-tools does not have the same behaviour.
+  #
+  # This hack works around this. This behaviour might be fixed by the time you
+  # read this. You can check by commenting out this block and running the
+  # accessibility and performance tests. You are encouraged to remove this hack
+  # as soon as it is no longer needed.
+  #
+  if defined?(Webpacker) && Rails.env.test?
+    # manifest paths depend on your webpacker config so we inspect it
+    manifest_path = Webpacker::Configuration
+                    .new(root_path: Rails.root, config_path: Rails.root.join("config/webpacker.yml"), env: Rails.env)
+                    .public_manifest_path
+                    .relative_path_from(Rails.root.join("public"))
+                    .to_s
+    get "/asset-manifest.json", to: redirect(manifest_path)
+  end
+
+  ##
+  # If you want the Sidekiq web console in production environments you need to
+  # put it behind some authentication first.
+  #
+  if defined?(Sidekiq::Web) && Rails.env.development?
+    mount Sidekiq::Web => "/sidekiq" # Sidekiq monitoring console
+    require "sidekiq/web"
+  end
+
+  root "home#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
