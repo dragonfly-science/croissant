2954e025dfcb46b743d9d64b984b2f0c78475de1
dan morrison <dan@rabidtech.co.nz>
2019-12-17 12:21:56 +1300

    add tests over tag helper methods

--- spec/helpers/tag_helper_spec.rb
+++ spec/helpers/tag_helper_spec.rb
@@ -4,18 +4,24 @@ describe TagHelper, type: :helper do
   describe "#tag_colours" do
     it "returns the correct list of tag colours" do
       colour_array = %w[primary info success olive warning orange danger pink purple violet]
-      expect(described_class.tag_colours).to eq(colour_array)
+      expect(helper.tag_colours).to eq(colour_array)
     end
   end
 
   describe "#tag_colour(index)" do
     context "when provided with an single digit index argument" do
       it "returns the colour at that index of the tag_colours array" do
+        expect(helper.tag_colour(0)).to eq("primary")
+        expect(helper.tag_colour(4)).to eq("warning")
+        expect(helper.tag_colour(9)).to eq("violet")
       end
     end
 
     context "when provided with an index argument greater than one digit" do
-      it "returns the colour " do
+      it "uses the arguments last digit to return a colour" do
+        expect(helper.tag_colour(20)).to eq("primary")
+        expect(helper.tag_colour(34)).to eq("warning")
+        expect(helper.tag_colour(559)).to eq("violet")
       end
     end
   end
