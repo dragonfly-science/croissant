37fdf9cbfaa86b3d15c42706ea1cd685a8c7987f
dan morrison <dan@rabidtech.co.nz>
2019-12-17 10:28:21 +1300

    adapt new tag spec to use new layout and add from taxonomy show page

--- spec/features/tags/new_tag_spec.rb
+++ spec/features/tags/new_tag_spec.rb
@@ -7,18 +7,9 @@ RSpec.feature "Creating a new tag", js: true do
     it "allows creating a new tag from the consultation" do
       visit consultation_path(consultation)
       click_link("Taxonomy")
-      click_link("New tag")
       fill_in("Name", with: "Trees")
-      click_button("Create Tag")
-      expect(page).to have_selector("li", text: "Trees")
+      click_button("✔")
+      expect(page).to have_selector("button", text: "Trees")
     end
-  end
-
-  context "the new tag page" do
-    before do
-      visit new_taxonomy_tag_path(consultation.taxonomy)
-    end
-
-    it_behaves_like "an accessible page"
   end
 end
