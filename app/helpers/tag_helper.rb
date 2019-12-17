4bc861ee19c99bff25665ba81105fb64168e5fd9
dan morrison <dan@rabidtech.co.nz>
2019-12-17 14:00:10 +1300

    use modulo instead of .digits to determine tag colour in helper

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -4,7 +4,7 @@ module TagHelper
   def tag_colour(index)
     # there are only 10 colours available for tags so we only care about
     # index values between 0 and 9 when we access them.
-    colour_index = index.digits.first
+    colour_index = index % TAG_COLOURS.length
     TAG_COLOURS[colour_index]
   end
 end
