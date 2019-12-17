98e4e615b45d71f22093012f95410e70cf9834f6
dan morrison <dan@rabidtech.co.nz>
2019-12-17 14:46:39 +1300

    remove old comment from tag helper

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -2,8 +2,6 @@ module TagHelper
   TAG_COLOURS = %w[primary info success olive warning orange danger pink purple violet].freeze
 
   def tag_colour_for_index(index)
-    # there are only 10 colours available for tags so we only care about
-    # index values between 0 and 9 when we access them.
     colour_index = index % TAG_COLOURS.length
     TAG_COLOURS[colour_index]
   end
