3e1b49ee1dc11248f0411b0cc2ce6e93301fa0be
dan morrison <dan@rabidtech.co.nz>
2019-12-17 14:02:36 +1300

    rename tag_colour helper method to tag_colour_for_index

--- spec/helpers/tag_helper_spec.rb
+++ spec/helpers/tag_helper_spec.rb
@@ -8,20 +8,20 @@ describe TagHelper, type: :helper do
     end
   end
 
-  describe "#tag_colour(index)" do
+  describe "#tag_colour_for_index(index)" do
     context "when provided with an single digit index argument" do
       it "returns the colour at that index of the tag_colours array" do
-        expect(helper.tag_colour(0)).to eq("primary")
-        expect(helper.tag_colour(4)).to eq("warning")
-        expect(helper.tag_colour(9)).to eq("violet")
+        expect(helper.tag_colour_for_index(0)).to eq("primary")
+        expect(helper.tag_colour_for_index(4)).to eq("warning")
+        expect(helper.tag_colour_for_index(9)).to eq("violet")
       end
     end
 
     context "when provided with an index argument greater than one digit" do
       it "uses the arguments last digit to return a colour" do
-        expect(helper.tag_colour(20)).to eq("primary")
-        expect(helper.tag_colour(34)).to eq("warning")
-        expect(helper.tag_colour(559)).to eq("violet")
+        expect(helper.tag_colour_for_index(20)).to eq("primary")
+        expect(helper.tag_colour_for_index(34)).to eq("warning")
+        expect(helper.tag_colour_for_index(559)).to eq("violet")
       end
     end
   end
