2119aebdef80ba1048ce29409fbdd2fc9a9cc247
dan morrison <dan@rabidtech.co.nz>
2020-01-29 09:47:16 +1300

    add condition to selectHTML function in taggableSubmissionText js so that multiple tags can be added sequentially to the same piece of selected text

--- app/frontend/modules/taggable_submission_text.js
+++ app/frontend/modules/taggable_submission_text.js
@@ -8,32 +8,40 @@ class TaggableSubmissionText {
 
     $('.submission-tag').on('click', function() {
       selectHTML($(this).data("tag-id"));
-      // add tag pressed text content as data attr to span
-      // get start and end character
-      // send tag id + start and end character to endpoint for persistance
     });
 
     function selectHTML(tagId) {
       try {
-        let previousSibling = getSelection().anchorNode.previousElementSibling;
+        const selection = getSelection();
+        let selectionStart = 0
+        let selectionEnd = 0
+        const sameSelection = (selection.anchorNode.id === "js-submissionText")
 
-        let siblingOffset = 0;
-        if (previousSibling !== null) {
-          siblingOffset = parseInt(previousSibling.dataset.endCharacter, 10);
+        if (sameSelection) {
+          const currentNode = selection.anchorNode.childNodes[selection.anchorOffset]
+          selectionStart = currentNode.dataset.startCharacter
+          selectionEnd = currentNode.dataset.endCharacter
+        } else {
+          let previousSibling = selection.anchorNode.previousElementSibling;
+
+          let siblingOffset = 0;
+          if (previousSibling !== null) {
+            siblingOffset = parseInt(previousSibling.dataset.endCharacter, 10);
+          }
+          // anchorOffset is where the user started the selection
+          selectionStart = selection.anchorOffset + siblingOffset;
+
+          if (previousSibling !== null) {
+            selectionStart += 1;
+          }
+          // focusOffset is the character after the selections end
+          selectionEnd = selection.focusOffset + siblingOffset;
+          if (previousSibling === null) {
+            selectionEnd -= 1;
+          }
         }
 
-        // anchorOffset is where the user started the selection
-        let selectionStart = getSelection().anchorOffset + siblingOffset;
-        if (previousSibling !== null) {
-          selectionStart += 1;
-        }
-        // focusOffset is the character after the selections end
-        let selectionEnd = getSelection().focusOffset + siblingOffset;
-        if (previousSibling === null) {
-          selectionEnd -= 1;
-        }
-
-        const text = getSelection().toString();
+        const text = selection.toString();
         const startChar = Math.min(selectionStart, selectionEnd);
         const endChar = Math.max(selectionStart, selectionEnd);
         const submissionId = $('.submission__taggable-text')[0].dataset
@@ -51,7 +59,7 @@ class TaggableSubmissionText {
 
         persistSubmissionTag(params);
       } catch (e) {
-        return getSelection();
+        return selection;
       }
     }
 
