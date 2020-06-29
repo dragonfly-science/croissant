8b24b3ea47d59bbc6264832793014a19a1320d47
Conrad Johnston <conrad@darnoc.co.nz>
2020-06-29 17:43:30 +1200

    Move url generation function to parent template in preperation for sharing

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -4,13 +4,25 @@ FactoryBot.define do
     description { "A lovely submission" }
     file { Rack::Test::UploadedFile.new("spec/support/example_files/00988_Anonymous.pdf", "application/pdf") }
 
+    trait :incoming do
+      text { "Taggable submission text" }
+      state { :incoming }
+    end
     trait :ready_to_tag do
       text { "Taggable submission text" }
       state { :ready }
     end
+    trait :started do
+      text { "Taggable submission text" }
+      state { :started }
+    end
     trait :finished do
       text { "Taggable submission text" }
       state { :finished }
+    end
+    trait :archived do
+      text { "Taggable submission text" }
+      state { :archived }
     end
     trait :with_survey do
       survey
